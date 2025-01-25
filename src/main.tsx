3935b29b249fed6888610bbfe5a6fe6d9eb63b26
nathanrael <ralaivoavy.natanael@gmail.com>
2025-01-25 08:29:48 +0300

    fix : attempt to fix vercel analytics not wrking

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.tsx'
 import {BrowserRouter as Router} from "react-router-dom"
 import { Analytics } from "@vercel/analytics/react"
+import { inject } from '@vercel/analytics';
+
+inject(); // Initialize Vercel Analytics
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
